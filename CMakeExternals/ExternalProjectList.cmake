84fa35aefdbfc0b70ffc904d67a8b21703f72a50
Ashish Singh <ashish.singh@uphs.upenn.edu>
2020-01-27 09:37:38 -0500

    adding yaml-cpp so it appears in cmake

--- CMakeExternals/ExternalProjectList.cmake
+++ CMakeExternals/ExternalProjectList.cmake
@@ -1,1 +1,2 @@
 mitkFunctionAddExternalProject(NAME GuidelinesSupportLibrary ON DOC "Use Microsoft's implementation of the Guidelines Support Library (GSL)")
+mitkFunctionAddExternalProject(NAME yaml-cpp ON DOC "Use YAML Cpp Library")
