4fa56adc1a7f06a288ab06ca275d677cd633118b
Dimitris Bounias <bounias.dimitris@gmail.com>
2020-01-27 19:11:35 -0500

    Fix issue #33: Remove GSL

--- CMakeExternals/ExternalProjectList.cmake
+++ CMakeExternals/ExternalProjectList.cmake
@@ -1,2 +1,1 @@
-mitkFunctionAddExternalProject(NAME GuidelinesSupportLibrary ON DOC "Use Microsoft's implementation of the Guidelines Support Library (GSL)")
 mitkFunctionAddExternalProject(NAME yaml-cpp ON DOC "Use YAML Cpp Library")
