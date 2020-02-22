4683e59b9fbeb9045f1083e32a9889bda9958b54
Dimitris Bounias <bounias.dimitris@gmail.com>
2020-02-22 00:13:52 -0500

    Fix various minor issues

--- CMake/CaPTkInstallRules.cmake
+++ CMake/CaPTkInstallRules.cmake
@@ -1,5 +1,5 @@
 # Install CaPTk ico
 
 MITK_INSTALL(FILES
-  "${CMAKE_CURRENT_LIST_DIR}/../Applications/CaPTkApp/icons/icon.ico"
+  "${CMAKE_CURRENT_LIST_DIR}/../Plugins/upenn.cbica.captk.application/resources/icon.png"
 )
